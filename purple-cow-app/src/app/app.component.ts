ea9cdfe6271e393774c6bb2c66ea74664005c73a
Eric <esaari@kion.io>
2022-02-19 10:18:33 -0500

    Test api call

--- purple-cow-app/src/app/app.component.ts
+++ purple-cow-app/src/app/app.component.ts
@@ -1,6 +1,6 @@
 declare var require: any;
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 const countapi = require('countapi-js');
 
@@ -9,10 +9,20 @@ const countapi = require('countapi-js');
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'purple-cow-app';
+  domain: string = 'ews';
+  key = '1ccb732e-b55a-4404-ad3f-0f99c02fe44e';
+  count: number = 0;
 
   ngOnInit() {
+    countapi.get(this.domain, this.key).then((result) => {
+      this.count = result.value;
+      console.log(this.count);
+    }, err => {
+      console.log(err);
+      alert('Unable to get current hit count');
+    });
 
   }
 }
