a54425cc4a3df7f0677bae821a3a220b52a1f2ab
Eric <esaari@kion.io>
2022-02-19 10:24:01 -0500

    button click event

--- purple-cow-app/src/app/counter-update/counter-update.component.ts
+++ purple-cow-app/src/app/counter-update/counter-update.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-counter-update',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CounterUpdateComponent implements OnInit {
 
+  @Output() onButtonClick: EventEmitter<string> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  buttonClicked() {
+    this.onButtonClick.emit('');
+  }
 }
